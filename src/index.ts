83aeb55bea82e9f27963009f25338d5b68571903
Juan D.K <66712975+Juan-d-khusuma@users.noreply.github.com>
2021-09-26 13:17:09 +0700

    Created the boilerplate

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => console.log(`Server started at port:${port}`));
