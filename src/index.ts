703cec6e086bdc50239d768a52647c78dce40e46
Juan D.K <66712975+Juan-d-khusuma@users.noreply.github.com>
2021-09-27 12:15:37 +0700

    Added basic graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import * as express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs } from "../graphql/schemas/schema";
+import { resolvers } from "../graphql/resolvers/resolver";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.listen(port, () => console.log(`Server started at port:${port}`));
+/**
+ * Start Apollo GraphQL server
+ */
+export async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+  app.listen({ port }, () =>
+    console.log(
+      `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+    )
+  );
+}
+startServer();
